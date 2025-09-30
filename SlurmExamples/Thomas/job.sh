c0ca61481255d13154cffb206dd3631d30acfc56
gantz-thomas-gantz <gantz.thomas.gantz@gmail.com>
2025-09-30 21:23:28 +0200

    added test job

---
+++ SlurmExamples/Thomas/job.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=00:15:00
+#SBATCH --qos=default
+#SBATCH --partition=gpu
+#SBATCH --account=p200981   # Your project ID
+#SBATCH --nodes=1           # Number of nodes
+#SBATCH --ntasks=1          # Number of tasks
+#SBATCH --ntasks-per-node=1 # Tasks per node
+
+# Print job info
+echo "Date              = $(date)"
+echo "Hostname          = $(hostname -s)"
+echo "Working Directory = $(pwd)"
+
+# Load necessary modules
+module add Apptainer
+
+# Pull and run the container
+apptainer pull docker://ollama/ollama
+apptainer exec --nv ollama_latest.sif ollama serve
+
